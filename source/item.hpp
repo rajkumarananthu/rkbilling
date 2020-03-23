cac9a0dccb9abf083824ad3409aada36ac4233f1
Rajkumar Ananthu <rajkumar.ananthu108@gmail.com>
2020-03-23 16:40:06 +0530

    updated the spec_document

---
+++ source/item.hpp
@@ -0,0 +1,12 @@
+#ifndef RK_ITEM_HPP
+#define RK_ITEM_HPP
+
+// a class that describe an the item, that can be purchased from the store
+class cItem {
+    private:
+        // to store the name of an item
+        std::string mName;
+	// to store the cost of an item
+        double mCost;
+};
+#endif // RK_ITEM_HPP
