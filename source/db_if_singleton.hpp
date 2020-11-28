9d3b5a320e859fabe632c96ce3388865e2134fad
Rajkumar Ananthu <rajkumar.ananthu108@gmail.com>
2020-11-28 18:14:21 +0530

    Added DB singleton

--- source/db_if_singleton.hpp
+++ source/db_if_singleton.hpp
@@ -1,7 +1,48 @@
 #ifndef RK_DB_SINGLETON_HPP
 #define RK_DB_SINGLETON_HPP
 
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+// A singleton class to access the MySQL database
+// uses libmysqlcppconn-dev package for this
 class cDBSingleton {
+
+    private:
+
+        static std::shared_ptr<cDBSingleton> mDB;
+        std::shared_ptr<sql::Driver> mDriver;
+        std::shared_ptr<sql::Connection> mCon;
+        std::shared_ptr<sql::Statement> mStmt;
+        std::shared_ptr<sql::ResultSet> mResSet;
+
+        ItemList mItemList; 
+
+        cDBSingleton() {
+            mDriver = nullptr;
+            mCon = nullptr;
+            mStmt = nullptr;
+            mResSet = nullptr;
+        }
+
+    public:
+
+        ~cDBSingleton() {
+            delete mDB;
+        }
+
+        static std::shared_ptr<cDBSingleton> getInstance() {
+            if (mDB == nullptr) {
+                mDB = new cDBSingleton();
+            }
+            return mDB;
+        }
+
+
 };
 
 #endif // RK_DB_SINGLETON_HPP
