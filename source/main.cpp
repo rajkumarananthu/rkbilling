35762ab614da8f52f78f4981b8ecf0bf7ddf658f
Rajkumar Ananthu <rajkumar.ananthu108@gmail.com>
2020-11-29 21:54:48 +0530

    Added size to window.

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@ int main (int argc, char *argv[]) {
     auto app = Gtk::Application::create(argc, argv, "org.rkbilling.top");
 
     cTopWindow topWindow;
+    topWindow.set_default_size(1200,600);
 
     return app->run(topWindow);
 }
